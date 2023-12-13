f9911a8b4ef7f261830dca77263d48a7d27d2df4
jeikei-anhhoang1420 <lehuuhoanganhhg2003@gmail.com>
2023-12-13 09:48:50 +0700

    login&register admin

--- views/auth/adminlogin.php
+++ views/auth/adminlogin.php
@@ -11,17 +11,19 @@
             <p class="font-bold"><i class="fa-solid fa-check"></i> Registered successfully</p>
             <p>Continue logging in to use the application</p>
         </div>
-    <?php }?> 
-    <div class="max-w-[1300px] lg:w-10/12 w-full min-h-screen lg:m-5 flex overflow-hidden lg:rounded-xl shadow-xl">
+    <?php } ?>
+    <div class="max-w-[1300px] lg:w-10/12 w-full min-h-screen lg:m-5 flex overflow-hidden">
         <div class="w-1/2 h-full overflow-hidden hidden lg:block">
-            <img src="https://images.unsplash.com/photo-1539109136881-3be0616acf4b?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZmFzaGlvbnxlbnwwfHwwfHx8MA%3D%3D" alt="">
+            <img src="/path/to/main2.jpg" alt="">
         </div>
-        <form action="/admin/login" method="POST" class="lg:w-1/2 flex justify-center items-center bg-[#00BFFF] w-full lg:p-3">
-            <div class="sm:w-[450px] w-[90%] bg-[#fff] flex flex-col justify-center rounded-md p-6 shadow-xl gap-y-3">
-                <h1 class="font-bold text-center text-[25px]">Admin Log In</h1>
-
-                <label for="" class="font-medium text-[16px]">Your email</label>
-                <input name="email" id="email" type="text" required autofocus placeholder="name@gmail.com" class="<?= isset($errors['email']) ? 'border-red-500' : '' ?> border-[#333] block text-[#333] border p-2 focus:outline-none focus:border-sky-700 focus:ring-sky-700 rounded-lg" name="email" value="<?= isset($old['email']) ? $this->e($old['email']) : '' ?>">
+        <form action="/admin/login" method="POST" class="lg:w-1/2 flex justify-center items-center w-full lg:p-3">
+            <div class="sm:w-[450px] w-[90%] bg-[#fff] flex flex-col justify-center rounded-md p-6 shadow-xl lg:shadow-md gap-y-3">
+                <div class="mb-2">
+                    <h1 class="font-bold text-[27px]">JK Admin Access</h1>
+                    <p class="text-[13px] text-[#333f48] font-medium">Please fill your detail to access your account.</p>
+                </div>
+                <label for="" class="font-medium text-[16px]">Email</label>
+                <input name="email" id="email" type="text" required autofocus placeholder="name@gmail.com" class="<?= isset($errors['email']) ? 'border-red-500' : '' ?> block text-[#333f48] border border-[#333f48] p-[6px] focus:outline-none focus:border-sky-700 focus:ring-sky-700 rounded-md" name="email" value="<?= isset($old['email']) ? $this->e($old['email']) : '' ?>">
 
                 <?php if (isset($errors['email'])) : ?>
                     <span class="text-red-500">
@@ -31,17 +33,17 @@
                 <?php endif ?>
 
                 <label for="password" class="font-medium text-[16px]">Password</label>
-                <input name="password" id="password" type="password" placeholder="••••••••" class=" <?= isset($errors['password']) ? 'border-red-500' : '' ?> block text-[#333] border border-[#333] p-2 focus:outline-none focus:border-sky-700 focus:ring-sky-700 rounded-lg">
+                <input name="password" id="password" type="password" placeholder="••••••••" class=" <?= isset($errors['password']) ? 'border-red-500' : '' ?> block text-[#333f48] border border-[#333f48] p-[6px] focus:outline-none focus:border-sky-700 focus:ring-sky-700 rounded-md">
                 <?php if (isset($errors['password'])) : ?>
                     <span class="text-red-500">
                         <i class="fa-solid fa-triangle-exclamation"></i>
                         <strong><?= $this->e($errors['password']) ?></strong>
                     </span>
                 <?php endif ?>
-                <button class="mt-4 block bg-[#00BFFF] p-2 font-bold rounded-2xl transition-all duration-300 hover:bg-[#2ea9d2] focus:ring focus:bg-[#4169E1] active:bg-[#4169E1]">Login</button>
-                <div class="flex items-center gap-1">
-                    <p class="text-[15px] text-[#333]">Don’t have an account yet?</p>
-                    <a href="/admin/register" class="text-[16px] text-[#4169E1] font-semibold cursor-pointer hover:underline">Register</a>
+                <button class="block bg-[#5429FF] text-[#fff] mt-4 p-2 font-bold rounded-sm transition-all duration-300 hover:bg-[#2ea9d2] focus:ring focus:bg-[#4169E1] active:bg-[#4169E1]">Sign in</button>
+                <div class="flex items-center gap-1 justify-center">
+                    <p class="text-[14px] text-[#333] font-medium">Don’t have an account yet?</p>
+                    <a href="/admin/register" class="text-[15px] text-[#5429FF] font-semibold cursor-pointer hover:underline">Register</a>
                 </div>
 
 
--- views/auth/adminregister.php
+++ views/auth/adminregister.php
@@ -1,12 +1,15 @@
 <?php $this->layout("layouts/auth", ["title" =>  "Admin register"]) ?>
 <?php $this->start("page") ?>
 <div class="w-full min-h-screen flex justify-center items-center">
-    <div class="max-w-[1300px] lg:w-10/12 w-full min-h-screen lg:m-5 flex overflow-hidden lg:rounded-xl shadow-xl">
-        <form method="POST" action="/admin/register" class="lg:w-1/2 flex justify-center items-center bg-[#FFD700] w-full lg:p-3">
-            <div class="sm:w-[450px] w-[90%] bg-[#fff] flex flex-col justify-center rounded-md p-6 shadow-xl gap-y-3">
-                <h1 class="font-bold text-center text-[25px]">Admin Register</h1>
+    <div class="max-w-[1300px] lg:w-10/12 w-full min-h-screen lg:m-5 flex overflow-hidden">
+        <form method="POST" action="/admin/register" class="lg:w-1/2 flex justify-center items-center w-full lg:p-3">
+            <div class="sm:w-[450px] w-[90%] bg-[#fff] flex flex-col justify-center rounded-md p-6 shadow-xl lg:shadow-md gap-y-3">
+                <div class="mb-2">
+                    <h1 class="font-bold text-[27px]">JK Admin Register</h1>
+                    <p class="text-[13px] text-[#333f48] font-medium">Please fill your detail to access your account.</p>
+                </div>
                 <label for="name" class="font-medium text-[16px]">Your name</label>
-                <input name="name" value="<?= isset($old['name']) ? $this->e($old['name']) : '' ?>" required autofocus type="text" placeholder="Jackson Micheal" class="<?= isset($errors['name']) ? 'border-red-500' : '' ?> block text-[#333] border border-[#333] p-2 focus:outline-none focus:border-yellow-500 focus:ring-yellow-500 rounded-lg">
+                <input name="name" value="<?= isset($old['name']) ? $this->e($old['name']) : '' ?>" required autofocus type="text" placeholder="Jackson Micheal" class="<?= isset($errors['name']) ? 'border-red-500' : '' ?> block text-[#333f48] border border-[#333f48] p-[6px] focus:outline-none focus:border-sky-700 focus:ring-sky-700 rounded-md">
                 <?php if (isset($errors['name'])) : ?>
                     <span class="text-red-500">
                         <i class="fa-solid fa-triangle-exclamation"></i>
@@ -15,7 +18,7 @@
                 <?php endif ?>
 
                 <label for="email" class="font-medium text-[16px]">Your email</label>
-                <input name="email" value="<?= isset($old['email']) ? $this->e($old['email']) : '' ?>" required autofocus type="email" placeholder="name@gmail.com" class="<?= isset($errors['email']) ? 'border-red-500' : '' ?> block text-[#333] border border-[#333] p-2 focus:outline-none focus:border-yellow-500 focus:ring-yellow-500 rounded-lg">
+                <input name="email" value="<?= isset($old['email']) ? $this->e($old['email']) : '' ?>" required autofocus type="email" placeholder="name@gmail.com" class="<?= isset($errors['email']) ? 'border-red-500' : '' ?> block text-[#333f48] border border-[#333f48] p-[6px] focus:outline-none focus:border-sky-700 focus:ring-sky-700 rounded-md">
                 <?php if (isset($errors['email'])) : ?>
                     <span class="text-red-500">
                         <i class="fa-solid fa-triangle-exclamation"></i>
@@ -25,7 +28,7 @@
 
 
                 <label for="password" class="font-medium text-[16px]">Password</label>
-                <input type="password" placeholder="••••••••" id="password" name="password" required class="<?= isset($errors['password']) ? ' border-red-500' : '' ?> block text-[#333] border border-[#333] p-2 focus:outline-none focus:border-yellow-500 focus:ring-yellow-500 rounded-lg">
+                <input type="password" placeholder="••••••••" id="password" name="password" required class="<?= isset($errors['password']) ? ' border-red-500' : '' ?> block text-[#333f48] border border-[#333f48] p-[6px] focus:outline-none focus:border-sky-700 focus:ring-sky-700 rounded-md">
                 <?php if (isset($errors['password'])) : ?>
                     <span class="text-red-500">
                         <i class="fa-solid fa-triangle-exclamation"></i>
@@ -35,22 +38,22 @@
 
 
                 <label for="password-confirm" class="font-medium text-[16px]">Confirm Password</label>
-                <input placeholder="••••••••" id="password-confirm" type="password" name="password_confirmation" required class="<?= isset($errors['password_confirmation']) ? 'border-red-500' : '' ?> block text-[#333] border border-[#333] p-2 focus:outline-none focus:border-yellow-500 focus:ring-yellow-500 rounded-lg">
+                <input placeholder="••••••••" id="password-confirm" type="password" name="password_confirmation" required class="<?= isset($errors['password_confirmation']) ? 'border-red-500' : '' ?> block text-[#333f48] border border-[#333f48] p-[6px] focus:outline-none focus:border-sky-700 focus:ring-sky-700 rounded-md">
                 <?php if (isset($errors['password'])) : ?>
                     <span class="text-red-500">
                         <i class="fa-solid fa-triangle-exclamation"></i>
                         <strong><?= $this->e($errors['password']) ?></strong>
                     </span>
                 <?php endif ?>
-                <button class="block bg-[#FFD700] p-2 font-bold rounded-2xl transition-all duration-300 hover:bg-[#FFFF00] focus:ring focus:bg-[#FFFF00] active:bg-[#FFFF00]">Sign Up</button>
-                <div class="flex items-center gap-1">
-                    <p class="text-[15px] text-[#333]">If you have an account?</p>
-                    <a href="/admin/login" class="text-[16px] text-[#FFD700] font-semibold cursor-pointer hover:underline">Log in</a>
+                <button class="block bg-[#5429FF] text-[#fff] mt-4 p-2 font-bold rounded-sm transition-all duration-300 hover:bg-[#2ea9d2] focus:ring focus:bg-[#4169E1] active:bg-[#4169E1]">Sign Up</button>
+                <div class="flex items-center gap-1 justify-center">
+                    <p class="text-[14px] font-medium text-[#333]">If you have an account?</p>
+                    <a href="/admin/login" class="text-[15px] text-[#5429FF] font-semibold cursor-pointer hover:underline">Log in</a>
                 </div>
             </div>
         </form>
         <div class="w-1/2 h-full overflow-hidden hidden lg:block">
-            <img src="https://images.unsplash.com/premium_photo-1694607587276-c0ef403b943c" alt="">
+            <img src="../../main2.jpg" alt="">
         </div>
     </div>
 </div>
